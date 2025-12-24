880bc06778a5b8a30150770ba717a94f9f0d24c8
Saiinaruto <saipraveenabburi7@gmail.com>
2025-12-24 09:43:49 +0530

    changed the hero section and added the resposonsive for the header

--- script.js
+++ script.js
@@ -87,3 +87,13 @@ profileCards.forEach(card => {
   });
 });
 
+
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
